5ce118c4309f9520579dd50b52750a778fffee7f
QuangDo <qdangdo@gmail.com>
2022-06-12 19:24:23 -0500

    setup heroku server

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+<!-- git subtree push --prefix server heroku main -->
 # glorified-bucket-list
--- client/src/utils/url.ts
+++ client/src/utils/url.ts
@@ -1,5 +1,5 @@
 const development = "http://localhost:4000";
-const production = "https://glorified-bucket-list.herokuapp.com";
+const production = "https://glorified-bucket-list-server.herokuapp.com";
 
 export const API_URL =
     process.env.NODE_ENV === "development" ? development : production;
