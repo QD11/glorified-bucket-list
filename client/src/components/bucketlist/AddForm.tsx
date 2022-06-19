1afd98ac758e80140e338c652d48d243c3644753
QuangDo <qdangdo@gmail.com>
2022-06-19 02:07:58 -0500

    edit forms

--- client/src/components/bucketlist/AddForm.tsx
+++ client/src/components/bucketlist/AddForm.tsx
@@ -1,4 +1,5 @@
-import { Box, TextField } from "@mui/material";
+import { Box } from "components/styled/elements/box";
+import { Form } from "components/styled/elements/form";
 import { useState } from "react";
 
 export const AddForm = () => {
@@ -10,59 +11,21 @@ export const AddForm = () => {
     });
 
     return (
-        <Box
-            component="form"
-            sx={{
-                "& .MuiTextField-root": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <TextField
-                required
-                id="outlined-required"
-                label="Title"
-                defaultValue=""
-            />
-            <TextField
-                disabled
-                id="outlined-disabled"
-                label="Disabled"
-                defaultValue="Hello World"
-            />
-            <TextField
-                id="outlined-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-            />
-            <TextField
-                id="outlined-read-only-input"
-                label="Read Only"
-                defaultValue="Hello World"
-                InputProps={{
-                    readOnly: true,
-                }}
-            />
-            <TextField
-                id="outlined-number"
-                label="Number"
-                type="number"
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-            <TextField
-                id="outlined-search"
-                label="Search field"
-                type="search"
-            />
-            <TextField
-                id="outlined-helperText"
-                label="Helper text"
-                defaultValue="Default Value"
-                helperText="Some important text"
-            />
+        <Box outline={"1px solid black"}>
+            <Form>
+                <label>
+                    Title:
+                    <input></input>
+                </label>
+                <label>
+                    Description:
+                    <input></input>
+                </label>
+                <label>
+                    Date:
+                    <input></input>
+                </label>
+            </Form>
         </Box>
     );
 };
---
+++ client/src/components/styled/elements/form.ts
@@ -0,0 +1,87 @@
+import { BoxBaseProps } from "components/styled/elements/box";
+import styled from "styled-components";
+
+export const Form = styled.form<BoxBaseProps>`
+    padding: ${(props) => (props.padding && `${props.padding}rem`) || null};
+    margin: ${(props) => (props.margin && `${props.margin}rem`) || null};
+    margin-top: ${(props) =>
+        (props.marginTop && `${props.marginTop}rem`) || null};
+    margin-bottom: ${(props) =>
+        (props.marginBottom && `${props.marginBottom}rem`) || null};
+    margin-right: ${(props) =>
+        (props.marginRight && `${props.marginRight}rem`) || null};
+    margin-left: ${(props) =>
+        (props.marginLeft && `${props.marginLeft}rem`) || null};
+    padding-top: ${(props) =>
+        (props.paddingTop && `${props.paddingTop}rem`) || null};
+    padding-bottom: ${(props) =>
+        (props.paddingBottom && `${props.paddingBottom}rem`) || null};
+    padding-right: ${(props) =>
+        (props.paddingRight && `${props.paddingRight}rem`) || null};
+    padding-left: ${(props) =>
+        (props.paddingLeft && `${props.paddingLeft}rem`) || null};
+    display: ${(props) => (props.flex && "flex") || null};
+    flex: ${(props) => props.flexProp || null};
+    border: ${(props) => props.border || null};
+    outline: ${(props) => props.outline || null};
+    overflow: ${(props) => props.overflow || null};
+    overflow-x: ${(props) => props.overflowX || null};
+    overflow-y: ${(props) => props.overflowY || null};
+    flex-direction: ${(props) => (props.column && "column") || null};
+    justify-content: ${(props) => props.justifyContent || null};
+    align-items: ${(props) =>
+        (props.centerV && "center") || props.alignItems || null};
+    gap: ${(props) =>
+        (props.gap && "5px") ||
+        (props.gapSize && `${props.gapSize}rem`) ||
+        null};
+    min-width: ${(props) => props.minWidth || null};
+    min-height: ${(props) => props.minHeight || null};
+    max-height: ${(props) => props.maxHeight || null};
+    width: ${(props) => props.width || null};
+    height: ${(props) => props.height || null};
+    min-width: ${(props) => props.minWidth};
+    cursor: ${(props) => props.cursor || null};
+    color: ${(props) => props.color || null};
+    background-color: ${(props) => props.backgroundColor || null};
+    border-radius: ${(props) =>
+        (props.borderRadius && `${props.borderRadius}rem`) || null};
+    position: ${(props) =>
+        (props.absolute && "absolute") ||
+        (props.sticky && "sticky") ||
+        (props.relative && "relative") ||
+        (props.static && "static") ||
+        "null"};
+    top: ${(props) => (props.top && props.top) || null};
+    right: ${(props) => (props.right && props.right) || null};
+    bottom: ${(props) => (props.bottom && props.bottom) || null};
+    left: ${(props) => (props.left && props.left) || null};
+    ${(props) =>
+        (props.card &&
+            `
+        background-color: white;
+        border-radius: 0.25rem;
+        box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%), 0 0px 0 1px rgb(10 10 10 / 2%);
+        color: #4a4a4a;
+        max-width: 100%;
+        position: relative;
+        width: 100%;
+        padding: 11px;
+        &:hover {
+          background-color: lightgrey;
+        }
+    `) ||
+        null};
+    ${(props) =>
+        (props.bulmaBox &&
+            `
+        background-color: white;
+        border-radius: 6px;
+        box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%), 0 0px 0 1px rgb(10 10 10 / 2%);
+        color: #4a4a4a;
+        display: block;
+        padding: 1.25rem;
+      `) ||
+        null};
+    writing-mode: ${(props) => props.writingMode};
+`;
